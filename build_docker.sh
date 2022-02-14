3f8228e83fda78f97e8529d93772cc36407b4a64
Yashu Liang <yashu.liang@sap.com>
2022-02-14 10:11:37 +0100

    changes

--- build_docker.sh
+++ build_docker.sh
@@ -2,3 +2,8 @@ echo "submitting to git"
 git add -A
 git commit -m "changes"
 git push origin master
+
+echo "builing and pushing Dockerfile"
+docker build -t dogsitter .
+docker image tag dogsitter pkid/dogsitter:latest
+docker image push pkid/dogsitter:latest
